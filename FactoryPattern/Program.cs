6413d79f2f94ff190b12d7f8f8bbf897012b3d4a
Daniel Finch <xiphoid0311@gmail.com>
2023-07-27 18:57:10 -0500

    Created a factory pattern vehicle method

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace FactoryPattern
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int wheelCount;
+            bool input = false;
+            do
+            {
+                Console.WriteLine("How many wheels we looking for?");
+                input = int.TryParse(Console.ReadLine(), out wheelCount);
+
+            }
+            while (input == false);
+
+            var modeOfTransportation = VehicleFactory.GetVehicle(wheelCount);
+            modeOfTransportation.Drive();                     
+            
         }
+        
     }
 }
---
+++ FactoryPattern/Unicycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Unicycle : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Your not getting very far on a Unicycle...");
+        }
+    }
+}
